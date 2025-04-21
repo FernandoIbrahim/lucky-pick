ef5800282673ee10a45935249ced205fe5639761
Fernando Antônio Ferreira Ibrahim <80763509+FernandoIbrahim@users.noreply.github.com>
2025-04-21 00:24:05 -0300

    renaming section

--- Code/web/src/components/home/guess-history-section.tsx
+++ Code/web/src/components/home/guess-history-section.tsx
@@ -9,7 +9,7 @@ export function GuessHistorySection({ guesses }: GuessHistorySectionProps) {
   if (!guesses || guesses.length === 0) return null;
   return (
     <section className="flex flex-col items-center w-full max-w-[500px] mx-auto mt-10 px-4">
-      <h2 className="text-2xl font-bold text-gray-800 mb-4">Match History</h2>
+      <h2 className="text-2xl font-bold text-gray-800 mb-4">Guess History</h2>
       <div className="flex flex-col  items-center gap-3 w-full max-h-[125px] overflow-y-scroll">
         {guesses.map((guess, index) => (
           <GuessHistoryItem key={index} guess={guess} />
